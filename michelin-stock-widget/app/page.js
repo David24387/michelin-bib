1d87dd8af35b78132836066ccb08a7c68409d666
David24387 <david.mueller@euromaster.com>
2026-05-12 13:34:33 +0200

    Update page.js

--- michelin-stock-widget/app/page.js
+++ michelin-stock-widget/app/page.js
@@ -1,15 +1,5 @@
 import MichelinStockWidget from "./components/MichelinStockWidget";
 
 export default function Page() {
-  return (
-    <main style={{
-      minHeight: "100vh",
-      display: "grid",
-      placeItems: "center",
-      background: "#f3f6fb",
-      padding: 24
-    }}>
-      <MichelinStockWidget />
-    </main>
-  );
+  return <MichelinStockWidget />;
 }
